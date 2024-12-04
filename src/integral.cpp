aeae643f54295d410088abdebe2e61c6035b4236
EugeneFayn <fayneugen@yandex.ru>
2024-12-04 13:47:34 +0300

    UPD finding integral, need to define calcF

--- src/integral.cpp
+++ src/integral.cpp
@@ -3,31 +3,36 @@
 #include "geometry/geometry.h"
 #include "quadrature/quadrature.h"
 
-std::complex<double> calcF(Vertex Xk, Vertex Ym, Vertex Cx, Vertex Cy, double wavenumber){
+// Добавить волновое число правильное
+std::complex<double> calcF(std::complex<double> Xk, std::complex<double> Ym, Vertex Cx, Vertex Cy, double wavenumber=1){
     std::complex<double> RES(0.0, 0.0);
-    double R = Xk.abs(Ym);
-    std::complex<double> POW(0)
-    RES = -4 + wavenumber * wavenumber * (Cx - Xk) * (Cy -Ym) * exp(-)
+    double R = abs(Xk - Ym);
+    std::complex<double> POW(0.0, wavenumber * R);
+    RES = -4 + wavenumber * wavenumber * (Cx - Xk) * (Cy - Ym) * exp(POW)/R;
     return RES;
 }
+
 // Достать список весов отдельно вынести
 std::complex<double> FindI(Triangle sigmaX,Triangle sigmaY,Vertex Cx, Vertex Cy){
     double wK[4]{-9/16, 25/48, 25/48, 25/48}; // Четырёхточечный Гаусс. Веса.
     double wM[3]{1/3, 1/3, 1/3}; // Трёхточечный Гаусс. Веса.
+    std::complex<double> Xk;
+    std::complex<double> Ym;
     std::complex<double> RES = sigmaX.calcSquare() * sigmaY.calcSquare();
     std::complex<double> SUM_(0.0,0.0);
     // Формирование интеграла
     for (int k=0;k<4;k++){
         for (int m=0;m<3;m++){
-            f
-
+            SUM_ = SUM_ + wK[k] * wM[m] * calcF(Xk,Ym,Cx,Cy);
         }
     }
+    RES = RES * SUM_;
+    return RES;
 };
 
 
-// spisok Edges [0,1,2,3]
-// spisok Triangles [0-,0+,1-,1+]
+// spisok Edges [0, 1, 2, 3]
+// spisok Triangles [0-, 0+, 1-,1+, ]
 //
 
 
