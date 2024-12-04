52dfbcea9d7cd625d857758593a8520d6a35d0c3
EugeneFayn <fayneugen@yandex.ru>
2024-12-04 17:03:12 +0300

    FIX integral.cpp

--- src/integral.cpp
+++ src/integral.cpp
@@ -3,11 +3,14 @@
 #include "geometry/geometry.h"
 #include "quadrature/quadrature.h"
 #include "cmath"
-
+// добавить eps=10-6
 // Добавить волновое число правильное
 std::complex<double> calcF_Aij(Vertex Xk, Vertex Ym, Vertex Cx, Vertex Cy, double wavenumber=1){
     std::complex<double> RES(0.0, 0.0);
     double R = Xk.distance(Ym);
+    if (R<1e-6){
+        return 0;
+    }
     Vertex Point1 = Cx - Xk; // Xk в квадратуре
     Vertex Point2 = Cy - Ym; // Ym в квадратуре
     double scalar = Point1.scalar_product(Point2);
@@ -20,10 +23,10 @@ std::complex<double> calcF_Aij(Vertex Xk, Vertex Ym, Vertex Cx, Vertex Cy, doubl
 std::complex<double> FindI_Aij(Triangle sigmaX,Triangle sigmaY,Vertex Cx, Vertex Cy){
     double wK[4]{-9/16, 25/48, 25/48, 25/48}; // Четырёхточечный Гаусс. Веса.
     double wM[3]{1/3, 1/3, 1/3}; // Трёхточечный Гаусс. Веса.
-    double ksi4[4]{1/3, 3/5, 1/5, 1/5};
-    double eta4[4]{1/3, 1/5, 3/5, 1/5};
-    double ksi3[3]{1/6, 2/3, 1/6};
-    double eta3[3]{1/6, 1/6, 2/3};
+    double ksi4[4]{1/3, 3/5, 1/5, 1/5}; // ksi четырёхточчный Гаусс.
+    double eta4[4]{1/3, 1/5, 3/5, 1/5}; // eta четырёхточечны Гаусс.
+    double ksi3[3]{1/6, 2/3, 1/6}; // ksi трёхточечный Гаусс.
+    double eta3[3]{1/6, 1/6, 2/3}; // eta трёхточечны Гаусс.
     std::complex<double> RES = sigmaX.calc_S() * sigmaY.calc_S();
     std::complex<double> SUM_(0.0,0.0);
     // Формирование интеграла
@@ -42,12 +45,12 @@ std::complex<double> FindI_Aij(Triangle sigmaX,Triangle sigmaY,Vertex Cx, Vertex
 // NxN - размер матриц
 // Чекнуть нумерацию рёбер с нуля или нет. В Edge начинается с нуля
 // triangles - массив треугольников
-void BuildMatrix(std::complex<double>** &A, int N, int b, Triangle* &triangles){
+void BuildMatrix(std::complex<double>* &A, int N, int b, Triangle* &triangles){
     // Идём по I,J.
     // В случае если I,J равны 0    
     double coef = 0.0;
     for (int i=0; i < N; i++){
-        for (int j=0; j < N + 1; j++){
+        for (int j=0; j < N; j++){
             std::complex<double> sum_(0.0,0.0);
             // p=1,q=1
             coef = 1 / (4 * M_PI * triangles[2 * i].calc_S() * triangles[2 * j].calc_S());
@@ -62,7 +65,7 @@ void BuildMatrix(std::complex<double>** &A, int N, int b, Triangle* &triangles){
             coef = 1 / (4 * M_PI * triangles[2 * i + 1].calc_S() * triangles[2 * j + 1].calc_S());
             sum_ += coef * FindI_Aij(triangles[2 * i + 1],triangles[2 * j + 1], triangles[2 * i + 1].getC() , triangles[2 * j + 1].getC());
             // Записываем результат в A_ij
-            A[i][j] = sum_;
+            A[i * N  + j] = sum_;
         }
     }
 }
